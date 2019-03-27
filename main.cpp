007ee4132cccf414dd621e1f707cb76dd628b621
David Saada <david.saada@arm.com>
2019-03-27 14:57:37 +0200

    Renovate SD example:
    - Add mount and format if mount fails
    - Use LittleFS instead of FAT
    - Format with astyle

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include "mbed.h"
-#include "FATFileSystem.h"
+#include "LittleFileSystem.h"
 #include "SDBlockDevice.h"
 #include <stdio.h>
 #include <errno.h>
@@ -8,77 +8,97 @@
 #include "platform/mbed_retarget.h"
 
 
-SDBlockDevice sd(MBED_CONF_SD_SPI_MOSI, 
-				 MBED_CONF_SD_SPI_MISO, 
-				 MBED_CONF_SD_SPI_CLK, 
-				 MBED_CONF_SD_SPI_CS);
-FATFileSystem fs("sd", &sd);
+SDBlockDevice sd(MBED_CONF_SD_SPI_MOSI,
+                 MBED_CONF_SD_SPI_MISO,
+                 MBED_CONF_SD_SPI_CLK,
+                 MBED_CONF_SD_SPI_CS);
+LittleFileSystem fs("sd", &sd);
 
-void return_error(int ret_val){
-  if (ret_val)
-    printf("Failure. %d\n", ret_val);
-  else
-    printf("done.\n");
+void return_error(int ret_val)
+{
+    if (ret_val) {
+        printf("Failure. %d\n", ret_val);
+        while (true) {
+            __WFI();
+        }
+    } else {
+        printf("done.\n");
+    }
 }
 
-void errno_error(void* ret_val){
-  if (ret_val == NULL) {
-    printf(" Failure. %d \n", errno);
-    while (true) __WFI();
-  } else
-    printf(" done.\n");
+void errno_error(void *ret_val)
+{
+    if (ret_val == NULL) {
+        printf(" Failure. %d \n", errno);
+        while (true) {
+            __WFI();
+        }
+    } else {
+        printf(" done.\n");
+    }
 }
 
 int main()
 {
-	int error = 0;
-	printf("Welcome to the filesystem example.\n");
+    int error = 0;
+    printf("Welcome to the filesystem example.\n");
 
-	printf("Opening a new file, numbers.txt.");
-	FILE* fd = fopen("/sd/numbers.txt", "w+");
-	errno_error(fd);
+    // Try to mount the filesystem
+    printf("Mounting the filesystem... ");
 
-	for (int i = 0; i < 20; i++){
-		printf("Writing decimal numbers to a file (%d/20)\r", i);
-		fprintf(fd, "%d\n", i);
-	}
-	printf("Writing decimal numbers to a file (20/20) done.\n");
+    error = fs.mount(&sd);
+    if (error) {
+        // Reformat if we can't mount the filesystem
+        // this should only happen on the first boot
+        printf("No filesystem found, formatting... ");
+        error = fs.reformat(&sd);
+        return_error(error);
+    }
+    printf("Opening a new file, numbers.txt.");
+    FILE *fd = fopen("/sd/numbers.txt", "w+");
+    errno_error(fd);
 
-	printf("Closing file.");
-	fclose(fd);
-	printf(" done.\n");
+    for (int i = 0; i < 20; i++) {
+        printf("Writing decimal numbers to a file (%d/20)\r", i);
+        fprintf(fd, "%d\n", i);
+    }
+    printf("Writing decimal numbers to a file (20/20) done.\n");
 
-	printf("Re-opening file read-only.");
-	fd = fopen("/sd/numbers.txt", "r");
-	errno_error(fd);
+    printf("Closing file.");
+    fclose(fd);
+    printf(" done.\n");
 
-	printf("Dumping file to screen.\n");
-	char buff[16] = {0};
-	while (!feof(fd)){
-		int size = fread(&buff[0], 1, 15, fd);
-		fwrite(&buff[0], 1, size, stdout);
-	}
-	printf("EOF.\n");
+    printf("Re-opening file read-only.");
+    fd = fopen("/sd/numbers.txt", "r");
+    errno_error(fd);
 
-	printf("Closing file.");
-	fclose(fd);
-	printf(" done.\n");
+    printf("Dumping file to screen.\n");
+    char buff[16] = {0};
+    while (!feof(fd)) {
+        int size = fread(&buff[0], 1, 15, fd);
+        fwrite(&buff[0], 1, size, stdout);
+    }
+    printf("EOF.\n");
 
-	printf("Opening root directory.");
-	DIR* dir = opendir("/sd/");
-	errno_error(fd);
+    printf("Closing file.");
+    fclose(fd);
+    printf(" done.\n");
 
-	struct dirent* de;
-	printf("Printing all filenames:\n");
-	while((de = readdir(dir)) != NULL){
-		printf("  %s\n", &(de->d_name)[0]);
-	}
+    printf("Opening root directory.");
+    DIR *dir = opendir("/sd/");
+    errno_error(fd);
 
-	printf("Closeing root directory. ");
-	error = closedir(dir);
-	return_error(error);
-	printf("Filesystem Demo complete.\n");
+    struct dirent *de;
+    printf("Printing all filenames:\n");
+    while ((de = readdir(dir)) != NULL) {
+        printf("  %s\n", &(de->d_name)[0]);
+    }
 
-	while (true) {}
+    printf("Closing root directory. ");
+    error = closedir(dir);
+    return_error(error);
+    printf("Filesystem Demo complete.\n");
+
+    while (true) {}
 }
 

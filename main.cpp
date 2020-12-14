55d3e118851a14f1e59aa46afc5139e2f26b4345
Harrison Mutai <harrison.mutai@arm.com>
2020-12-14 12:47:29 +0000

    Fix prebuild and compile invocations of mbedtools
    Build and checkout are invalid invocations of mbedtools since they were
    removed in the recent update. Replace checkout with deploy to setup an
    mbed project, and compile to build the project.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 /* mbed Microcontroller Library
- * Copyright (c) 2019 ARM Limited
+ * Copyright (c) 2020-2021 Arm Limited and Contributors. All rights reserved.
+ *
  * SPDX-License-Identifier: Apache-2.0
  */
 #include "mbed.h"
